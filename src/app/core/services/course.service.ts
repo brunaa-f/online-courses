1cfdf15d2b81e3dbb0d6bf44bd1dae892815b5cc
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-11 15:58:39 -0300

    feat: create list component and implement filters

--- src/app/core/services/course.service.ts
+++ src/app/core/services/course.service.ts
@@ -1,33 +1,70 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course.model';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
 
+  private coursesSubject = new BehaviorSubject<Course[]>([]);
+  courses$ = this.coursesSubject.asObservable();
 
-  private courses: Course[] = [
-    {
-      id: 1,
-      name: 'Curso de Angular',
-      description: 'Aprenda Angular do básico ao avançado.',
-      category: 'Programação',
-      duration: 40,
-      image: 'assets/design.png',
-      lessons: [
-      { id: 1, title: 'Introdução ao Angular', description: 'Primeiros passos com Angular', duration: 30 },
-      { id: 2, title: 'Componentes e Diretivas', description: 'Entendendo a estrutura básica do Angular', duration: 45 }
-      ]
-    },
-  ];
+  private filtersSubject = new BehaviorSubject<{ category: string; search: string }>({
+    category: '',
+    search: ''
+  });
+  filters$ = this.filtersSubject.asObservable();
 
-  getCourses(): Observable<Course[]> {
-    return of(this.courses); 
+  constructor() {
+    this.loadCourses();
   }
 
-  getCourseById(id: number): Course | undefined {
-    return this.courses.find(course => course.id === id);
+  private loadCourses() {
+    const courses: Course[] = [
+      {
+        id: 1,
+        name: 'React',
+        description: 'Aprenda ',
+        category: 'Programação',
+        duration: 40,
+        image: 'assets/b.webp',
+        lessons: [
+        { id: 1, title: 'Introdução ao react', description: 'Primeiros passos', duration: 30 },
+        { id: 2, title: 'Redux', description: 'Entendendo a estrutura básica', duration: 45 }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Curso de Angular',
+        description: 'Aprenda Angular do básico ao avançado.',
+        category: 'Programação',
+        duration: 40,
+        image: 'assets/a.webp',
+        lessons: [
+        { id: 1, title: 'Introdução ao angular', description: 'Primeiros passos com Angular', duration: 30 },
+        { id: 2, title: 'Componentes e Diretivas', description: 'Entendendo a estrutura básica do Angular', duration: 45 }
+        ]
+      },
+    ];
+    this.coursesSubject.next(courses);
+  }
+
+  setFilters(filters: { category: string; search: string }) {
+    this.filtersSubject.next(filters);
+  }
+
+  getFilteredCourses() {
+    return combineLatest([this.courses$, this.filters$]).pipe(
+      map(([courses, filters]) => {
+        const { category, search } = filters;
+        return courses.filter(
+          (course) =>
+            (category === '' || course.category === category) && 
+            course.name.toLowerCase().includes(search.toLowerCase()) 
+        );
+      })
+    );
   }
 }
--- src/app/features/course-list/course-list.component.ts
+++ src/app/features/course-list/course-list.component.ts
@@ -1,37 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';  
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { CourseService } from '../../core/services/course.service';
 import { Course } from '../../core/models/course.model';
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-course-list',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-  title = "Grade de Cursos"
-  isAuthenticated: boolean = true; 
-  courses: Course[] = [];
-  filteredCourses: Course[] = [];
-  searchQuery: string = '';
+  title = "Grade de Cursos";
+  isAuthenticated: boolean = true;
+  filters = { category: '', search: '' };
+  filteredCourses$: Observable<Course[]> = new Observable<Course[]>(); 
 
+  constructor(private courseService: CourseService) { }
 
-  constructor(private courseService: CourseService) {}
-
-  ngOnInit(): void {
-    this.courseService.getCourses().subscribe(data => {
-      this.courses = data;
-      this.filteredCourses = data;
-      console.log(this.courses)
-    });
+  ngOnInit() {
+    this.filteredCourses$ = this.courseService.getFilteredCourses();
   }
 
-  searchCourses(): void {
-    this.filteredCourses = this.courses.filter(course =>
-      course.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
+  updateFilters() {
+    this.courseService.setFilters(this.filters);
   }
 }
