942d2200a2b281f99a16605ea8acf40e49272ffe
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-11 16:51:54 -0300

    feat: create component not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CourseListComponent } from './features/course-list/course-list.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'courses', component: CourseListComponent },
-  { path: '', redirectTo: '/courses', pathMatch: 'full' } 
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
