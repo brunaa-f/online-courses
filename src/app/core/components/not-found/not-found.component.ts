8abda1efb889fd0bd21b75daed4f814139335d2b
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-14 11:28:39 -0300

    fix: not found title

--- src/app/core/components/not-found/not-found.component.ts
+++ src/app/core/components/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import lottie from 'lottie-web';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
@@ -11,14 +12,22 @@ import lottie from 'lottie-web';
 export class NotFoundComponent {
   @ViewChild('lottieContainer', { static: true }) lottieContainer!: ElementRef;
 
+  constructor(
+    private titleService: Title
+  ) { }
+
   ngAfterViewInit() {
     lottie.loadAnimation({
-      container: this.lottieContainer.nativeElement, 
-      renderer: 'svg', 
-      loop: true, 
-      autoplay: true, 
-      path: 'assets/404-animation.json', 
+      container: this.lottieContainer.nativeElement,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/404-animation.json',
     });
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Not Found');
   }
 
 }
